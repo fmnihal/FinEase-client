cda66c7239947b439fa379206672328eac0cb962
fmnihal <fattahmahmudnihal@gmail.com>
2025-11-12 01:25:00 +0600

    firebase

---
+++ firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCA8gCpCfVIujCk32p-z8yqMmOhG87Y6mU",
+  authDomain: "fin-ease-14e74.firebaseapp.com",
+  projectId: "fin-ease-14e74",
+  storageBucket: "fin-ease-14e74.firebasestorage.app",
+  messagingSenderId: "809743975125",
+  appId: "1:809743975125:web:610b2f6fc12ddb2396185c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
